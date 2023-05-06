2107d512058d7f941bc6ff3dfaf7f9a8da23fb05
minkyung00 <mksin00@naver.com>
2023-05-06 16:36:42 +0900

    chore: react-query provider 추가

---
+++ src/providers/TanstackQueryProvider.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { PropsWithChildren, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export default function TanstackQueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
