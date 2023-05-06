c1dcd83caacd4b57b2d00295f14917aa82859fab
minkyung00 <mksin00@naver.com>
2023-05-06 23:21:27 +0900

    chore: ReactQueryDevtools 추가

--- src/providers/TanstackQueryProvider.tsx
+++ src/providers/TanstackQueryProvider.tsx
@@ -2,9 +2,15 @@
 
 import { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function TanstackQueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
