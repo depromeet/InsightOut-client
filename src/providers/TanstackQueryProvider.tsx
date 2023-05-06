79900f8fbbec2634f90c89124b724bcf7ce4ad27
minkyung00 <mksin00@naver.com>
2023-05-06 23:41:26 +0900

    chore: prettier 세미콜론 제거 설정

--- src/providers/TanstackQueryProvider.tsx
+++ src/providers/TanstackQueryProvider.tsx
@@ -1,16 +1,16 @@
-'use client';
+'use client'
 
-import { PropsWithChildren, useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function TanstackQueryProvider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools />
     </QueryClientProvider>
-  );
+  )
 }
