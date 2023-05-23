635df7e7555e7aad41b5820900d22a71c3ec2f9c
highjoon <yundosa2@gmail.com>
2023-05-24 01:46:41 +0900

    feat: access token 만료 시 재발급 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,15 @@ import { Box } from '@chakra-ui/react';
 import useGoogleLogin from '@/hooks/useGoogleLogin';
 import { colors } from '@/styles/theme/foundations/colors';
 import { textStyles } from '@/styles/theme/foundations/textStyles';
+import instance from '@/apis';
 
 export default function Home() {
   const { onSuccess, onFailure } = useGoogleLogin();
+
+  const handleAnotherApi = async () => {
+    const resume = await instance.get('/resumes');
+    console.log(resume);
+  };
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
@@ -19,6 +25,7 @@ export default function Home() {
         onSuccess={onSuccess}
         onFailure={onFailure}
       />
+      <button onClick={handleAnotherApi}>테스트를 위해 다른 API 호출</button>
     </main>
   );
 }
--- src/components/providers/GoogleProvider.tsx
+++ src/components/providers/GoogleProvider.tsx
@@ -2,9 +2,14 @@
 
 import { useEffect } from 'react';
 import { gapi } from 'gapi-script';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+import authApi from '@/apis/auth';
 
 export default function GoogleLoginProvider({ children }: StrictPropsWithChildren) {
   useEffect(() => {
+    if (typeof window === 'undefined') return;
+
     function start() {
       gapi.client.init({
         clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -13,6 +18,18 @@ export default function GoogleLoginProvider({ children }: StrictPropsWithChildre
     }
 
     gapi.load('client:auth2', start);
+  }, []);
+
+  useEffect(() => {
+    const accessToken = axios.defaults.headers['Authorization'];
+    const refreshToken = getCookie('refreshToken');
+    if (accessToken || !refreshToken) return;
+
+    (async () => {
+      const response = await authApi.reIssue();
+      const accessToken = response.data.accessToken;
+      axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+    })();
   }, []);
 
   return <>{children}</>;
