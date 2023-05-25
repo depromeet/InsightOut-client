29e0a91e9bc683dcd5e86dad9681c468fd55af67
highjoon <yundosa2@gmail.com>
2023-05-26 00:02:27 +0900

    chore:api 주석 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -2,8 +2,17 @@ import { AxiosResponse } from 'axios';
 import instance from '.';
 
 const authApi = {
+  /**
+   *
+   * @param idToken 구글 Id Token
+   * @description 회원가입 또는 로그인하여 accessToken 발급
+   */
   signIn: (idToken: string) =>
     instance.post<string, AxiosResponse<{ accessToken: string }>>('/auth/signin', { idToken }),
+  /**
+   *
+   * @description cookie에 담겨있는 refreshToken을 활용하여 accessToken 재발급
+   */
   reIssue: () => instance.post<any, AxiosResponse<{ accessToken: string }>>('/auth/reissue'),
 };
 
