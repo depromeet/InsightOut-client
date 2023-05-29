78aed85699cb8078995d21c834d8d8b52ffab081
highjoon <yundosa2@gmail.com>
2023-05-29 16:58:14 +0900

    fix: firebase config 수정

--- src/hooks/useGoogleLogin.ts
+++ src/hooks/useGoogleLogin.ts
@@ -10,8 +10,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-} as const;
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
