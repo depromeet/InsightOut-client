b64d825f9fbcc33ef836125a261ff087dfdbd7cd
highjoon <yundosa2@gmail.com>
2023-05-29 16:54:21 +0900

    refactor: firebase config 이동

--- src/hooks/useGoogleLogin.ts
+++ src/hooks/useGoogleLogin.ts
@@ -2,13 +2,22 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import axios, { AxiosError } from 'axios';
 import authApi from '@/apis/auth';
-import { firebaseConfig } from '@/services/constants/firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+} as const;
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const useGoogleLogin = () => {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-
   const signIn = async () => {
     const response = await signInWithPopup(auth, provider);
     const idToken = await response.user.getIdToken();
