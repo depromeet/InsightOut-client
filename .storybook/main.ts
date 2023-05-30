68587156fff552036426c51f72c2d2e74d64246d
minkyung00 <mksin00@naver.com>
2023-05-30 23:47:36 +0900

    chore: storybook tailwind 관련 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,17 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
