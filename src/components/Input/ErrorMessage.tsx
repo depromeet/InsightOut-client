9e519f0b78d0ae0a95e0aac78b1ceb90310484d9
minkyung00 <mksin00@naver.com>
2023-05-30 23:50:34 +0900

    feat: ErrorIcon 추가

--- src/components/Input/ErrorMessage.tsx
+++ src/components/Input/ErrorMessage.tsx
@@ -1,3 +1,34 @@
 import { PropsWithChildren } from 'react';
 
-export const ErrorMessage = ({ children }: PropsWithChildren) => <p className="mt-2 text-xs text-error">{children}</p>;
+export const ErrorMessage = ({ children }: PropsWithChildren) => (
+  <p className="flex gap-[6px] mt-2 ml-3 text-xs text-error">
+    <ErrorIcon />
+    {children}
+  </p>
+);
+
+const ErrorIcon = () => (
+  <svg
+    className="stroke-error"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_1561_10855)">
+      <path
+        d="M8.00016 14.6663C11.6821 14.6663 14.6668 11.6816 14.6668 7.99967C14.6668 4.31778 11.6821 1.33301 8.00016 1.33301C4.31826 1.33301 1.3335 4.31778 1.3335 7.99967C1.3335 11.6816 4.31826 14.6663 8.00016 14.6663Z"
+        strokeWidth="1.7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M8 10.667H8.00667" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M8 5.33301V7.99967" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round" />
+    </g>
+    <defs>
+      <clipPath id="clip0_1561_10855">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
