bb54b5d17a4a331fcf8857ac2019747f027d4591
highjoon <yundosa2@gmail.com>
2023-06-01 00:46:37 +0900

    refactor: 컴포넌트 관련 타입 파일 통합

--- src/services/@types/component.d.ts
+++ src/services/@types/component.d.ts
@@ -1,4 +1,11 @@
 /* eslint-disable unused-imports/no-unused-vars */
 
+// Button
+
 type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'secondary-green' | 'outlined';
 type ButtonSize = 'sm' | 'md' | 'lg' | 'xl';
+
+// Chip
+
+type ChipVariant = 'primary' | 'secondary';
+type ChipSize = 'sm' | 'md';
