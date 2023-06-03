db39da204a1381b500f2774ec1ae07b4568be49a
highjoon <yundosa2@gmail.com>
2023-06-03 17:52:50 +0900

    chore: wrapTooltipChildren 함수 doc 추가

--- src/services/utils/wrapTooltipChildren.tsx
+++ src/services/utils/wrapTooltipChildren.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/**
+ * @name wrapTooltipChildren
+ * @description 문자열이 15자를 초과할 경우 줄바꿈으로 감싸는 함수
+ *
+ * @param {string} children - 감싸고자 하는 입력 문자열
+ * @returns {string | JSX.Element[]} - 줄바꿈이 적용된 문자열 또는 원본 문자열
+ */
 const wrapTooltipChildren = (children: string): string | JSX.Element[] => {
   const shouldWrapChildren = children.length > 15;
   const matches = children.match(/.{1,15}/g);
