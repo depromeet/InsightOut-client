e98073b94f09cc320a6e20640b2f5feb9f05a63e
minkyung00 <mksin00@naver.com>
2023-06-05 07:55:28 +0900

    feat: Icon 컴포넌트 수정

--- src/components/Icon/IconPlus.tsx
+++ src/components/Icon/IconPlus.tsx
@@ -5,14 +5,12 @@ import { tw } from '@/services/utils/tailwindMerge';
 const IconPlus = ({ className, ...props }: ComponentProps<'svg'>) => (
   <svg
     className={tw('stroke-gray-600', className)}
-    width="20"
-    height="21"
-    viewBox="0 0 20 21"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={21}
+    fill="none"
     {...props}>
-    <path d="M10 15.9844V4.31771" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M15.833 10.1509H4.16634" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 15.983V4.317M15.833 10.15H4.166" />
   </svg>
 );
 
