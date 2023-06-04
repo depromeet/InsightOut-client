b670edde5cd51247547ed6101534845b36aa3b60
sangbooom <tkdqja4164@naver.com>
2023-06-04 12:16:55 +0900

    refactor: push -> next/link

--- src/app/analyze/template.tsx
+++ src/app/analyze/template.tsx
@@ -5,27 +5,24 @@ import { LayoutProps } from './layout';
 import Button from '@/components/Button/Button';
 import { ROUTES } from '@/shared/constants/routes';
 import { usePathname, useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Template = ({ children }: LayoutProps) => {
   const pathname = usePathname();
-  const { push, back } = useRouter();
+  const { back } = useRouter();
 
-  const handleNextButton = () => {
+  const nextHref = () => {
     switch (pathname) {
       case ROUTES.EXPERIENCE:
-        push(ROUTES.INFORMATION);
-        break;
+        return ROUTES.INFORMATION;
       case ROUTES.INFORMATION:
-        push(ROUTES.KEYWORD);
-        break;
+        return ROUTES.KEYWORD;
       case ROUTES.KEYWORD:
-        push(ROUTES.VERIFY);
-        break;
+        return ROUTES.VERIFY;
       case ROUTES.VERIFY:
-        push(ROUTES.COMPLETE);
-        break;
+        return ROUTES.COMPLETE;
       default:
-        break;
+        return ROUTES.EXPERIENCE;
     }
   };
 
@@ -41,9 +38,11 @@ const Template = ({ children }: LayoutProps) => {
                 이전으로
               </Button>
             )}
-            <Button variant="primary" size="xl" onClick={handleNextButton}>
-              {pathname === ROUTES.VERIFY ? '경험카드 만들기' : '다음으로'}
-            </Button>
+            <Link href={nextHref()}>
+              <Button variant="primary" size="xl">
+                {pathname === ROUTES.VERIFY ? '경험카드 만들기' : '다음으로'}
+              </Button>
+            </Link>
           </div>
         </div>
         <div className="experience mb-[16px] px-[14px] py-[32px] h-[500px] top-[32px] sticky">{/** aside */}</div>
