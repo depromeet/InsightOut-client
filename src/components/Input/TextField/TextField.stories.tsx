f1db018a2a6dcd48a5d1e5bdf1e132d4605a9151
minkyung00 <mksin00@naver.com>
2023-06-04 22:18:04 +0900

    refactor: 스토리 수정

--- src/components/Input/TextField/TextField.stories.tsx
+++ src/components/Input/TextField/TextField.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Flex } from '@chakra-ui/react';
 
 import TextField from './TextField';
 
@@ -17,4 +18,22 @@ export const Default: Story = {
     mode: 'default',
     chipTitle: '나의 역할',
   },
+};
+
+export const Example: Story = {
+  render: () => (
+    <Flex flexDirection={'column'} gap={5}>
+      <Flex flexDirection={'column'} gap={10}>
+        <TextField placeholder="ex.개발자와 협업 역량을 쌓기 위해 it 동아리에 들어감" />
+        <TextField disabled placeholder="ex.개발자와 협업 역량을 쌓기 위해 it 동아리에 들어감" />
+        <TextField
+          error
+          errorMessage="답변을 작성해주세요!"
+          placeholder="ex.개발자와 협업 역량을 쌓기 위해 it 동아리에 들어감"
+        />
+        <TextField value="Typed TextField" />
+        <TextField readOnly value="Readonly TextField" />
+      </Flex>
+    </Flex>
+  ),
 };
