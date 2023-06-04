707b856aed0e7c8c7d84210d9c367813e0e1fe68
minkyung00 <mksin00@naver.com>
2023-06-04 13:11:31 +0900

    feat: LNB Question 컴포넌트

---
+++ src/feature/resume/components/LeftNavigationBar/Resume/ResumeItem/Question.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Question from './Question';
+
+const meta: Meta<typeof Question> = {
+  title: 'Components/LNB/Question',
+  component: Question,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Question>;
+
+export const Default: Story = {
+  render: (args) => <Question {...args} />,
+  args: {
+    id: 1,
+    title: '샘플) 자신의 경쟁력에 대해 구체적으로 작성해보세요.',
+  },
+};
