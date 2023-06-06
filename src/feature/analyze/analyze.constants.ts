ccb9d3e847ba7de58a9795069c60e32af0a98c79
sangbooom <tkdqja4164@naver.com>
2023-06-06 22:19:28 +0900

    feat: react-hook-form 초기 세팅 완료

---
+++ src/feature/analyze/analyze.constants.ts
@@ -0,0 +1,16 @@
+import { ExperienceFormValues } from './analyze.types';
+
+export const initialValue: ExperienceFormValues = {
+  experienceId: null,
+  title: '',
+  experienceRole: '',
+  motivation: '',
+  startDate: '',
+  endDate: '',
+  situation: '',
+  task: '',
+  action: '',
+  result: '',
+  experienceStatus: 'INPROGRESS',
+  utilization: '',
+};
---
+++ src/feature/analyze/analyze.types.ts
@@ -0,0 +1,52 @@
+type ExperienceStatus = 'INPROGRESS' | 'DONE';
+
+export interface ExperienceFormValues {
+  /**
+   * 경험 분해 키워드 가져올 때 필요한 id
+   */
+  experienceId: number | null;
+  /**
+   * 경험 제목 (/analyze/experience)
+   */
+  title: string;
+  /**
+   * 경험에서 내가 맡은 역할 (/analyze/experience)
+   */
+  experienceRole: string;
+  /**
+   * 경험을 하게 된 동기 (/analyze/experience)
+   */
+  motivation: string;
+  /**
+   * 경험 기간 시작 일자 (/analyze/experience)
+   */
+  startDate: string;
+  /**
+   * 경험 기간 종료 일자 (/analyze/experience)
+   */
+  endDate: string;
+  /**
+   * 경험 내용(STAR) 중 S (/analyze/information)
+   */
+  situation: string;
+  /**
+   * 경험 내용(STAR) 중 T (/analyze/information)
+   */
+  task: string;
+  /**
+   * 경험 내용(STAR) 중 A (/analyze/information)
+   */
+  action: string;
+  /**
+   * 경험 내용(STAR) 중 R (/analyze/information)
+   */
+  result: string;
+  /**
+   * 경험 카드 작성 상태
+   */
+  experienceStatus: ExperienceStatus;
+  /**
+   * TODO: 아직 안정해짐 (/analyze/verify)
+   */
+  utilization: string;
+}
