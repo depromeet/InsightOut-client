e59229dc60ab11efab6d804928e27f8d93365a5c
harseille <joonhabaak@gmail.com>
2023-06-07 05:54:25 +0900

    feat: page nav, contents 레이아웃 설정

--- src/app/collection/experiences/page.tsx
+++ src/app/collection/experiences/page.tsx
@@ -1,5 +1,55 @@
+import Badge from '@/components/Badge/Badge';
+import Chip from '@/components/Chip/Chip';
+
 const Page = () => {
-  return <div>Experience Page</div>;
+  const capabilities = [
+    {
+      id: 123,
+      title: '팀워크',
+      count: 4,
+    },
+    {
+      id: 124,
+      title: '리더십',
+      count: 10,
+    },
+    {
+      id: 125,
+      title: '분석능력',
+      count: 5,
+    },
+    {
+      id: 126,
+      title: '설득력',
+      count: 7,
+    },
+    {
+      id: 127,
+      title: '책임감',
+      count: 13,
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-row gap-[8px] mt-[24px]">
+        {capabilities.map(({ id, title, count }) => (
+          <Chip
+            key={id}
+            size="md"
+            variant="secondary"
+            badge={
+              <Badge style="tertiary" size="sm">
+                {count}
+              </Badge>
+            }>
+            {title}
+          </Chip>
+        ))}
+      </div>
+      <div className="mt-[24px]">contents</div>
+    </>
+  );
 };
 
 export default Page;
