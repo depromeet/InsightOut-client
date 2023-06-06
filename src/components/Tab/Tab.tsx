43f9441c980e198eb34ff4a5b69e323bdab9de90
Joonha Park <85827017+harseille@users.noreply.github.com>
2023-06-06 20:11:52 +0900

    fix: Link href type error

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 type TabProps = MergeComponentProps<
   'a',
   {
-    href: string;
+    href: UrlObject | RouteImpl<string>;
     /** @description 탭 크기 (xs, sm, md, lg) */
     size: TabSize;
     /** @description Badge 컴포넌트 */
