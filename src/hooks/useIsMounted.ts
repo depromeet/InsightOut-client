5b23be883e8155f450d44dfad87e6d549903c2bd
sangbooom <tkdqja4164@naver.com>
2023-06-06 22:20:11 +0900

    feat: useIsMounted 훅 구현

---
+++ src/hooks/useIsMounted.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export const useIsMounted = (): boolean => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  return isMounted;
+};
