2b994d70266fe753631327b3b224aff832c5387a
minkyung00 <mksin00@naver.com>
2023-06-07 21:00:18 +0900

    feat: 자기소개서 페이지 구조 설정

---
+++ src/app/resumes/[questionId]/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="flex flex-col min-w-[748px] border-[1px] border-purple-100 shadow-toast rounded-3xl bg-white py-[30px] px-11">
+      {/* 자기소개서 작성 부분 */}
+    </div>
+  );
+};
+
+export default Page;
