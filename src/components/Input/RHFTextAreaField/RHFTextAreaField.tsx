273c79358973a6f77f4c77b646332bdbc610b462
sangbooom <tkdqja4164@naver.com>
2023-06-10 10:52:40 +0900

    feat: useFormContext가 주입된 RhFTextAreaField 구현 (이번 pr에선 사용안할 예정)

--- src/components/Input/RHFTextAreaField/RHFTextAreaField.tsx
+++ src/components/Input/RHFTextAreaField/RHFTextAreaField.tsx
@@ -1,61 +1,80 @@
 'use client';
 
-import { ComponentPropsWithRef } from 'react';
+import React, { ComponentPropsWithRef, ForwardedRef, forwardRef } from 'react';
 
 import { tw } from '@/services/utils/tailwindMerge';
 
 import { ErrorMessage } from '../ErrorMessage';
 import { TextLengthMessage } from '../TextLengthMessage';
 import { FieldValues, Path, RegisterOptions, useFormContext } from 'react-hook-form';
+import { useForwardRef } from '@/hooks/useForwardRef';
+import { resizeHeight } from '@/services/utils/autoSizeTextarea';
+import Tag from '@/components/Tag/Tag';
 
 interface TextAreaFieldProps<TFieldValues extends FieldValues = FieldValues> extends ComponentPropsWithRef<'textarea'> {
-  showCount?: boolean;
+  chipTitle?: string;
   error?: boolean;
   errorMessage?: string;
   value?: string;
+  autoSize?: boolean;
 
   registerOption?: RegisterOptions<TFieldValues>;
-  name?: Path<TFieldValues>;
+  name: Path<TFieldValues>;
 }
 
 /**
- * @param showCount true일 경우에 글자 수가 보입니다.
  * @param error true일 경우에 텍스트 필드가 error 상태로 스타일링됩니다.
  * @param errorMessage error가 true인 경우에 사용. 텍스트 필드 하단에 보이는 메시지
  *
  * react-hook-form의 useFormContext가 주입된 TextAreaField
  */
-const RHFTextAreaField = <TFieldValues extends FieldValues>({
-  showCount,
-  value,
-  maxLength,
-  error,
-  errorMessage,
-  className,
-  registerOption,
-  name,
-  ...props
-}: TextAreaFieldProps<TFieldValues>) => {
+const RHFTextAreaField = forwardRef<HTMLTextAreaElement, TextAreaFieldProps>(function RHFTextAreaField<
+  TFieldValues extends FieldValues
+>(
+  {
+    chipTitle,
+    value,
+    maxLength,
+    error,
+    errorMessage,
+    className,
+    autoSize = false,
+    registerOption,
+    name,
+    ...props
+  }: TextAreaFieldProps<TFieldValues>,
+  ref: ForwardedRef<HTMLTextAreaElement>
+) {
+  const forwardRef = useForwardRef<HTMLTextAreaElement>(ref);
   const { register } = useFormContext<TFieldValues>();
+
+  const handleTextareaChange = () => autoSize && resizeHeight(forwardRef);
   return (
     <>
+      {chipTitle && (
+        <Tag variant="tertiary" size="s1" className="inline-block mb-2">
+          {chipTitle}
+        </Tag>
+      )}
       <textarea
+        ref={forwardRef}
         value={value}
         maxLength={maxLength}
-        cols={3}
-        className={tw(
-          `form h-[56px] max-h-[118px] resize-none ${value ? 'form-typed' : ''} ${error ? 'form-error' : ''}`,
-          className
-        )}
+        className={tw(`form resize-none ${error ? 'form-error' : ''}`, className)}
+        onChange={handleTextareaChange}
         {...(!!name && register(name, { ...registerOption }))}
         {...props}
       />
-      {showCount && (
+      {maxLength && (
         <TextLengthMessage className="float-right mt-2" currentLength={value?.length || 0} maxLength={maxLength || 0} />
       )}
-      {error && !value && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      {error && !value && <ErrorMessage hasIcon>{errorMessage}</ErrorMessage>}
     </>
   );
-};
+});
 
-export default RHFTextAreaField;
+const TextAreaFieldWithGeneric = RHFTextAreaField as <TFieldValues extends FieldValues = FieldValues>(
+  props: TextAreaFieldProps<TFieldValues> & React.RefAttributes<HTMLTextAreaElement>
+) => ReturnType<typeof RHFTextAreaField>;
+
+export default TextAreaFieldWithGeneric as typeof TextAreaFieldWithGeneric;
