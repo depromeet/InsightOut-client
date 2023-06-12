78fb41d37e4420c7d8817c02e40b389918444abc
highjoon <yundosa2@gmail.com>
2023-06-12 22:20:42 +0900

    chore: 스토리북 구글 로그인 버튼 컴포넌트명 수정

--- src/feature/auth/components/GoogleLoginButton.stories.tsx
+++ src/feature/auth/components/GoogleLoginButton.stories.tsx
@@ -9,6 +9,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof GoogleLoginButton>;
 
-export const _GlobalNavigationBar: Story = {
+export const _GoogleLoginButton: Story = {
   render: () => <GoogleLoginButton />,
 };
