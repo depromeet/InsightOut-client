ebab5f91804c7cc30a00881d63db385529b82a56
sangbooom <tkdqja4164@naver.com>
2023-06-16 03:39:16 +0900

    feat: AI 직무역량 페이지 퍼블리싱

---
+++ src/feature/analyze/verify/AICapabilityKeyword.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Tag from '@/components/Tag/Tag';
+import Alert from '@/../public/icons/alert-circle-line.svg';
+import { renderRecommendKeyword } from '@/app/analyze/verify/page';
+
+interface Props {
+  recommendKeywordList: string[];
+}
+
+const AICapabilityKeyword = ({ recommendKeywordList }: Props) => {
+  return (
+    <div className="flex flex-col flex-center border-[1px] border-secondary-400 rounded-[16px] bg-secondary-50 px-[22px] py-[32px] ">
+      <div className="w-[132px] h-[132px] mb-[7px]">132x132 img or lottie</div>
+      <div className="h6 break-keep mb-[16px]">
+        잠깐, OOO님! <br />
+        직무역량 키워드에 {renderRecommendKeyword(recommendKeywordList)}을 추가해보면 어때요?
+      </div>
+      <p className="b1 mb-[24px]">
+        올때메로나님의 경험을 AI가 분석한 결과 창의력과 협동력 역량을 추천하고 싶어요! 개발자, 기획자를 비롯해 여러
+        직군의 사람들과 협동하고, 서비스 출시라는 목표를 이뤄낸 과정에서 아래의 역량이 잘 드러나 추천하고 싶어요.
+        자기소개서와 면접 때 해당 역량을 어필해보는건 어떨까요?
+      </p>
+      <div className="flex gap-[12px] mb-[18px]">
+        {recommendKeywordList.map((keyword) => (
+          <Tag key={keyword} variant="secondary500" size="L">
+            {keyword}
+          </Tag>
+        ))}
+      </div>
+      <div className="flex-center">
+        <Alert />
+        <span className="c1">AI 역량 키워드는 최대 2개까지 추천돼요</span>
+      </div>
+    </div>
+  );
+};
+
+export default AICapabilityKeyword;
---
+++ src/feature/analyze/verify/SelectedKeywordContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tag from '@/components/Tag/Tag';
+
+const SelectedKeywordContainer = ({ children }: StrictPropsWithChildren) => {
+  return (
+    <div className="flex flex-col mt-[56px]">
+      <Tag variant="gray100" size="S" className="inline-block mb-2">
+        내가 선택한 경험 역략 키워드
+      </Tag>
+      <div className="flex gap-[8px]">{children}</div>
+    </div>
+  );
+};
+
+export default SelectedKeywordContainer;
