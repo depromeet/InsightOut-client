1dcb9622ffa1c8ea87e614d0aa77274dcefdde86
harseille <joonhabaak@gmail.com>
2023-06-17 01:20:30 +0900

    chore: badge variant명 변경

--- src/components/Icon/IconClock.tsx
+++ src/components/Icon/IconClock.tsx
@@ -14,9 +14,9 @@ const IconClock = ({ className, ...props }: ComponentProps<'svg'>) => (
       className="fill-none"
       d="M12 20.5C16.4183 20.5 20 16.9183 20 12.5C20 8.08172 16.4183 4.5 12 4.5C7.58172 4.5 4 8.08172 4 12.5C4 16.9183 7.58172 20.5 12 20.5Z"
       stroke="#525463"
-      stroke-width="1.8"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path d="M12 8.5V12.5L8.5 14" stroke="#525463" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
