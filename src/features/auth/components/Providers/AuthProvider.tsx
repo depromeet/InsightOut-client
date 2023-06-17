b79f06bcc6033c0c91c89fe5116a3b02e7684916
highjoon <yundosa2@gmail.com>
2023-06-17 16:18:40 +0900

    feat: Auth Provider 로그인 유지 로직 보완

--- src/features/auth/components/Providers/AuthProvider.tsx
+++ src/features/auth/components/Providers/AuthProvider.tsx
@@ -5,24 +5,30 @@ import axios from 'axios';
 import { getCookie } from 'cookies-next';
 import { useAuthActions } from '@/features/auth/store';
 import authApi from '@/apis/auth';
+import { usePathname } from 'next/navigation';
 
 export default function AuthProvider({ children }: StrictPropsWithChildren) {
+  const pathName = usePathname();
   const { setIsSignedIn } = useAuthActions();
 
   useEffect(() => {
     const accessToken = axios.defaults.headers['Authorization'];
     const refreshToken = getCookie('refreshToken');
-    if (accessToken && refreshToken) setIsSignedIn(true);
-    if (accessToken || !refreshToken) return;
+    if (accessToken) setIsSignedIn(true);
 
-    (async () => {
-      const response = await authApi.reIssue();
-      const accessToken = response.data.data.accessToken;
-      axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
-      setIsSignedIn(true);
-    })();
+    /**
+     * Access Token이 존재하지 않고 Refresh Token만 존재할 경우 Access Token 재발급
+     */
+    if (!accessToken && refreshToken) {
+      (async () => {
+        const response = await authApi.reIssue();
+        const accessToken = response.data.data.accessToken;
+        axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+        setIsSignedIn(true);
+      })();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [pathName]);
 
   return <>{children}</>;
 }
