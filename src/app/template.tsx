682a8bae3d66fe0482cf1c1fb440103fb229fc45
sangbooom <tkdqja4164@naver.com>
2023-06-18 17:32:10 +0900

    fix: AuthProvider 위치 이동 (리렌더링을 유발 하지 않는 layout에서 template로 이동)

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import GlobalNavigationBar from '@/components/GlobalNavigationBar/GlobalNavigationBar';
+import AuthProvider from '@/features/auth/components/Providers/AuthProvider';
 import useGoogleLogin from '@/features/auth/hooks/useGoogleLogin';
 
 export default function Template({ children }: StrictPropsWithChildren) {
   const { isSignedIn, signIn } = useGoogleLogin();
+
   return (
     <>
       <GlobalNavigationBar isSignedIn={isSignedIn} signIn={signIn} />
-      {children}
+      <AuthProvider>{children}</AuthProvider>
     </>
   );
 }
