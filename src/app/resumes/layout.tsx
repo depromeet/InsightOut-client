1073884869135a27d40424ee5f1ee14fee4ab865
minkyung00 <mksin00@naver.com>
2023-06-19 22:35:50 +0900

    feat: 자기소개서 조회 prefetch hydration

--- src/app/resumes/layout.tsx
+++ src/app/resumes/layout.tsx
@@ -1,29 +1,24 @@
 import { PropsWithChildren } from 'react';
 
 import Aside from '@/features/resume/components/Aside/Aside';
+import PrefetchHydration from '@/components/tanstackQuery/PrefetchHydration';
+import resumeApi from '@/features/resume/apis/resume';
+import { QUERY_KEY } from '@/shared/constants/queryKey';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  /** TODO: fetch('/resumes') */
-  const demoResponse = [
-    {
-      id: 1234,
-      title: '디프만 13기',
-      createdAt: '2023-06-05T05:58:44.402Z',
-      updatedAt: '2023-06-05T05:58:44.402Z',
-      questions: [
-        {
-          id: 1234,
-          title: '디프만 13기 지원동기',
-          answer: '디프만을 통한 빠른 성장',
-          updatedAt: '2023-06-05T05:58:44.402Z',
-        },
-      ],
-    },
-  ];
-
   return (
     <div className="flex h-full bg-gray-50">
-      <Aside resumeList={demoResponse} />
+      {/* @ts-expect-error Server Component */}
+      <PrefetchHydration
+        queryKey={[QUERY_KEY.resumes]}
+        queryFn={async () => {
+          const {
+            data: { data },
+          } = await resumeApi.get();
+          return data;
+        }}>
+        <Aside />
+      </PrefetchHydration>
       <div className="mx-[10px] mt-[19px]">{children}</div>
       <div className="mx-[10px] mt-[19px]">
         <section className="flex flex-col min-w-[699px] border-[1px] border-primary-100 shadow-toast rounded-3xl bg-white">
