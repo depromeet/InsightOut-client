997c7792c9af9dc53fc43004de19f5d4774e3021
sangbooom <tkdqja4164@naver.com>
2023-06-21 02:20:59 +0900

    refactor: Stepper 상수 정리

--- src/feature/analyze/constants/index.ts
+++ src/feature/analyze/constants/index.ts
@@ -1,3 +1,4 @@
+import { ROUTES } from '@/shared/constants/routes';
 import { ExperienceFormValues } from '../types';
 
 export const initialValue: ExperienceFormValues = {
@@ -13,3 +14,33 @@ export const initialValue: ExperienceFormValues = {
   result: '',
   experienceStatus: 'INPROGRESS',
 };
+
+export const STEP = {
+  experience: 1,
+  keyword: 2,
+  information: 3,
+  verify: 4,
+} as const;
+
+export const STEPS = [
+  {
+    id: STEP.experience,
+    title: '경험정보',
+    route: ROUTES.EXPERIENCE,
+  },
+  {
+    id: STEP.keyword,
+    title: '직무역량 키워드',
+    route: ROUTES.KEYWORD,
+  },
+  {
+    id: STEP.information,
+    title: '경험내용',
+    route: ROUTES.INFORMATION,
+  },
+  {
+    id: STEP.verify,
+    title: 'AI 직무역량 추천',
+    route: ROUTES.VERIFY,
+  },
+];
