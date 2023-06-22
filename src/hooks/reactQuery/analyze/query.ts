683488a0ff7686d6c26841fb0b8706997a760193
sangbooom <tkdqja4164@naver.com>
2023-06-23 00:45:15 +0900

    refactor: 불필요한 코드 제거

--- src/hooks/reactQuery/analyze/query.ts
+++ src/hooks/reactQuery/analyze/query.ts
@@ -5,21 +5,17 @@ import { ANALYZE_KEY } from '@/shared/constants/querykeys';
 import { ExperienceParams, ExperienceResponse } from '@/apis/analyze/types/experience';
 import experienceApi from '@/apis/analyze/experience';
 import { AxiosError } from 'axios';
-import { useIsSignedIn } from '@/features/auth/store';
 
 export const useFetchAnalyze = (
   params?: ExperienceParams['get'],
   options?: UseQueryOptions<ExperienceResponse, AxiosError>
 ) => {
-  const isSignIn = useIsSignedIn();
   return useQuery<ExperienceResponse, AxiosError>(
     ANALYZE_KEY.detail([{ ...params }]),
     () => experienceApi.get({ ...params }),
     {
       ...options,
       staleTime: Infinity,
-
-      enabled: isSignIn && options?.enabled,
       onSuccess(data) {
         options?.onSuccess?.(data);
       },
