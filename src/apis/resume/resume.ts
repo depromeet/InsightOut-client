e5a2474e627e0c52b413a712aaa57cfd9e51eb91
minkyung00 <mksin00@naver.com>
2023-06-24 15:23:25 +0900

    refactor: api 작성 방식 수정

--- src/apis/resume/resume.ts
+++ src/apis/resume/resume.ts
@@ -1,13 +1,14 @@
 import instance from '@/apis';
-import { ResumeParams } from './types/resume';
+import { ResumeParams, ResumeResponse } from './types/resume';
 
 const API_URL = '/resumes';
 
 const resumeApi = {
-  get: () => instance.get(API_URL),
-  post: () => instance.post(API_URL),
-  patch: ({ resumeId, payload }: ResumeParams['patch']) => instance.patch(`${API_URL}/${resumeId}`, payload),
-  delete: ({ resumeId }: ResumeParams['delete']) => instance.delete(`${API_URL}/${resumeId}`),
+  get: async () => await instance.get<ResumeResponse['get'], ResumeResponse['get']>(API_URL),
+  post: async () => await instance.post(API_URL),
+  patch: async ({ resumeId, payload }: ResumeParams['patch']) =>
+    await instance.patch(`${API_URL}/${resumeId}`, payload),
+  delete: async ({ resumeId }: ResumeParams['delete']) => await instance.delete(`${API_URL}/${resumeId}`),
 };
 
 export default resumeApi;
