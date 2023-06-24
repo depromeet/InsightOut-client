e64c131730cffb959555ed593294a9272434a0e8
sangbooom <tkdqja4164@naver.com>
2023-06-24 10:30:11 +0900

    refactor: 경험분해 feature에서 template 제거 => 리렌더링 이슈(언마운트, 마운트 됨)

--- src/app/analyze/layout.tsx
+++ src/app/analyze/layout.tsx
@@ -1,20 +1,53 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
+import { Route } from 'next';
+import Button from '@/components/Button/Button';
+import { ROUTES } from '@/shared/constants/routes';
+import { usePathname, useRouter } from 'next/navigation';
+import Stepper from '@/components/Stepper/Stepper';
 import { useIsMounted } from '@/hooks/useIsMounted';
 import { DevTool } from '@hookform/devtools';
 import { FormProvider, useForm } from 'react-hook-form';
-import { initialValue } from '@/feature/analyze/constants';
+import { STEPS, initialValue } from '@/feature/analyze/constants';
 import { ExperienceFormValues } from '@/feature/analyze/types';
+import TooltipRelativeContent from '@/components/Tooltip/TooltipRelativeContent';
+import Lottie from '@/components/Lottie/Lottie';
+import Progress from '@/components/Progress/Progress';
 
 export interface LayoutProps {
   children: React.ReactNode;
 }
 const Layout = ({ children }: LayoutProps) => {
+  const { back } = useRouter();
   const methods = useForm({
     mode: 'all',
     defaultValues: initialValue,
   });
+
+  const pathname = usePathname();
+  const tooltipIndex = (STEPS.find((v) => v.route === pathname)?.id ?? 1) - 1;
+
+  const TOOLTIP_CONTENTS = [
+    <>“OOO님 좋은 시작이에요”</>,
+    <>“뭐든지 시작이 반이에요”</>,
+    <>“내용을 풍부하게 작성할수록 다양한 AI 직무 역량 키워드를 받을 수 있어요 최선을 다해 작성해주세요 :)”</>,
+    <>“AI 직무역량 키워드 추천을 통해 나의 직무역량을 다각도로 넓혀보세요”</>,
+  ];
+
+  const handleNextButton = () => {
+    switch (pathname) {
+      case ROUTES.EXPERIENCE:
+        return ROUTES.KEYWORD;
+      case ROUTES.KEYWORD:
+        return ROUTES.INFORMATION;
+      case ROUTES.INFORMATION:
+        return ROUTES.VERIFY;
+      default:
+        return ROUTES.EXPERIENCE;
+    }
+  };
 
   const isMounted = useIsMounted();
 
@@ -25,7 +58,50 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(submit)}>
-        <div className="pt-[105px] mx-auto max-w-[1200px] h-[100%]">{children}</div>
+        <div className="pt-[105px] mx-auto max-w-[1200px] h-[100%]">
+          <div className="experience px-[90.5px] py-[40px] mb-[32px]">
+            <Stepper />
+          </div>
+          <div className="flex flex-row">
+            <div className="min-w-[770px] mr-[46px] mb-[32px]">
+              {children}
+              <div className="flex-center mt-[32px] gap-[12px]">
+                {pathname === ROUTES.EXPERIENCE ? null : (
+                  <Button type="button" variant="gray200" size="XL" onClick={back}>
+                    이전으로
+                  </Button>
+                )}
+                {pathname === ROUTES.VERIFY ? (
+                  <Button type="submit" variant="gray900" size="XL">
+                    경험카드 만들기
+                  </Button>
+                ) : (
+                  <Link href={handleNextButton() as Route}>
+                    <Button type="button" variant="gray900" size="XL">
+                      다음으로
+                    </Button>
+                  </Link>
+                )}
+              </div>
+            </div>
+
+            {/* Aside */}
+            <div className="experience flex flex-col items-center mb-[16px] px-[14px] py-[32px] h-[100%] top-[88px] sticky">
+              <div className="mb-[8px]">
+                <TooltipRelativeContent
+                  type="primary"
+                  position="center-top"
+                  content={TOOLTIP_CONTENTS[tooltipIndex]}
+                  isOpen
+                />
+              </div>
+              <div className="w-[200px] h-[200px] mb-[24px]">
+                <Lottie key="asd" src="/lotties/lumos-smiling.json" />
+              </div>
+              <Progress />
+            </div>
+          </div>
+        </div>
         {isMounted && <DevTool control={methods.control} />}
       </form>
     </FormProvider>
