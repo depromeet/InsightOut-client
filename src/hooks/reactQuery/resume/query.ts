8f2007b4fd2fb668546778fd57d24209cf548c25
minkyung00 <mksin00@naver.com>
2023-06-28 12:10:15 +0900

    fix: 쿼리 키 수정

--- src/hooks/reactQuery/resume/query.ts
+++ src/hooks/reactQuery/resume/query.ts
@@ -1,11 +1,10 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
-
-import { RESUME_KEY } from '@/shared/constants/querykeys';
 
 import resumeApi from '@/apis/resume/resume';
 import { ResumeResponse } from '@/apis/resume/types/resume';
+import { RESUME_KEY } from '@/shared/constants/querykeys';
 
 export const useGetResumes = (options?: UseQueryOptions<ResumeResponse['get'], AxiosError>) => {
-  return useQuery<ResumeResponse['get'], AxiosError>([RESUME_KEY.lists()], () => resumeApi.get(), { ...options });
+  return useQuery<ResumeResponse['get'], AxiosError>(RESUME_KEY.lists(), () => resumeApi.get(), { ...options });
 };
