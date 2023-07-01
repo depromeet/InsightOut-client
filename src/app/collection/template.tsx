ea45adeef00a088e917ac539c919350c13c19a93
harseille <joonhabaak@gmail.com>
2023-07-01 18:59:02 +0900

    feat: useGetExperienceCount 적용

--- src/app/collection/template.tsx
+++ src/app/collection/template.tsx
@@ -4,16 +4,27 @@ import { usePathname } from 'next/navigation';
 
 import Badge from '@/components/Badge/Badge';
 import Tab from '@/components/Tab/Tab';
+import { useGetExperienceCount } from '@/hooks/reactQuery/experience/qeury';
 import { COLLECTION_TABS } from '@/shared/constants/tabs';
 import addPlusMarkOver99 from '@/shared/utils/addPlusMarkOver99';
 
 const Template = ({ children }: { children: React.ReactNode }) => {
   const currentPath = usePathname();
 
+  const { data: experienceCount } = useGetExperienceCount();
+  //TODO: 추가 count API 적용
+  const resumeCount = 100;
+  const aiRecommendCount = 20;
+  const counts = [experienceCount?.experience || 0, resumeCount, aiRecommendCount];
+
+  const shownCollectionTabs = COLLECTION_TABS.map((tab, index) => {
+    return { ...tab, count: counts[index] };
+  });
+
   return (
     <div className="min-w-[1200px]">
       <div className="flex flex-row gap-5">
-        {COLLECTION_TABS.map(({ title, pathname, count }) => (
+        {shownCollectionTabs.map(({ title, pathname, count }) => (
           <Tab
             href={`/collection/${pathname}`}
             key={title}
