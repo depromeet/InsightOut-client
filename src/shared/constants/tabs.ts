913715bae97a82d30fecc1634dd965d8f6971d5f
harseille <joonhabaak@gmail.com>
2023-07-03 02:55:09 +0900

    feat: useGetResumesCount hook 적용

--- src/shared/constants/tabs.ts
+++ src/shared/constants/tabs.ts
@@ -1,5 +1,5 @@
 export const COLLECTION_TABS = [
-  { title: '경험카드', pathname: 'experiences', count: 0 },
-  { title: '자기소개서', pathname: 'resumes', count: 100 },
-  { title: 'AI 추천 자기소개서', pathname: 'ai-recommends', count: 50 },
+  { title: '경험카드', pathname: 'experiences' },
+  { title: '자기소개서', pathname: 'resumes' },
+  { title: 'AI 추천 자기소개서', pathname: 'ai-recommends' },
 ] as const;
