c528a4dd88001d4e895e9599a36ae77a7038f7f1
harseille <joonhabaak@gmail.com>
2023-07-03 00:38:35 +0900

    feat: useGetExperiences hook 추가

--- src/app/collection/experiences/page.tsx
+++ src/app/collection/experiences/page.tsx
@@ -4,264 +4,425 @@ import { useState } from 'react';
 
 import TextButton from '@/components/Button/TextButton';
 import IconClock from '@/components/Icon/IconClock';
-import ExperienceListCard from '@/features/collection/components/cards/ExperienceListCard/ExperienceListCard';
 import ChipListNav from '@/features/collection/components/nav/ChipListNav';
 import { EXPERIENCE_SORT_BY } from '@/features/collection/constants';
-import { Capability, Experience } from '@/features/collection/types';
+import { Capability } from '@/features/collection/types';
 import getAllCapability from '@/features/collection/utils/getAllCapabilityBadgeItem';
-import getFilteredExperiences from '@/features/collection/utils/getFilteredExperiences';
-import getSortedExperiences from '@/features/collection/utils/getSortedExperiences';
-import { useGetExperienceCapabilities } from '@/hooks/reactQuery/experience/qeury';
+import { useGetExperienceCapabilities, useGetExperiences } from '@/hooks/reactQuery/experience/qeury';
 
 const Page = () => {
   const { data: capabilities } = useGetExperienceCapabilities();
 
   const _capabilites = capabilities || [];
 
+  const { data: experiences } = useGetExperiences();
+
+  console.log(experiences);
+
   // TODO: 경험카드 스키마 전달 받고 적용
-  const experiences: Experience[] = [
-    {
-      id: 1,
-      title: '00전자 디자인 인턴',
-      startDate: '2022-02-01T00:00:00.000Z',
-      endDate: '2022-04-01T00:00:00.000Z',
-      updatedAt: '2023-04-01T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-        {
-          isAi: true,
-          id: 124,
-          keyword: '데이터분석',
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: '00물산 영업',
-      startDate: '2022-03-01T00:00:00.000Z',
-      endDate: '2022-04-01T00:00:00.000Z',
-      updatedAt: '2023-04-02T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1237,
-          keyword: '도전정신',
-        },
-        {
-          isAi: false,
-          id: 1238,
-          keyword: '고객지향성',
-        },
-        {
-          isAi: false,
-          id: 1239,
-          keyword: '기획력',
-        },
-        {
-          isAi: true,
-          id: 124,
-          keyword: '데이터분석',
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: '00전자 개발',
-      startDate: '2020-05-01T00:00:00.000Z',
-      endDate: '2020-08-01T00:00:00.000Z',
-      updatedAt: '2023-04-03T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: '00기획 마케팅',
-      startDate: '2023-01-01T00:00:00.000Z',
-      endDate: '2023-05-01T00:00:00.000Z',
-      updatedAt: '2023-04-04T00:00:00.000Z',
-      experienceStatus: 'DONE',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-      ],
-    },
-    {
-      id: 5,
-      title: '00은행 영업',
-      startDate: '2022-01-01T00:00:00.000Z',
-      endDate: '2022-03-01T00:00:00.000Z',
-      updatedAt: '2023-04-05T00:00:00.000Z',
-      experienceStatus: 'DONE',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-      ],
-    },
-    {
-      id: 6,
-      title: '00디스플레이 생산관리',
-      startDate: '2021-02-01T00:00:00.000Z',
-      endDate: '2021-04-01T00:00:00.000Z',
-      updatedAt: '2023-04-06T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-      ],
-    },
-    {
-      id: 7,
-      title: '00바이오 연구개발',
-      startDate: '2020-06-01T00:00:00.000Z',
-      endDate: '2021-12-01T00:00:00.000Z',
-      updatedAt: '2023-04-07T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-      ],
-    },
-    {
-      id: 8,
-      title: '00방송 콘텐츠 기획',
-      startDate: '2020-02-01T00:00:00.000Z',
-      endDate: '2020-04-01T00:00:00.000Z',
-      updatedAt: '2023-04-08T00:00:00.000Z',
-      experienceStatus: 'INPROGRESS',
-      situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
-      summaries: ['경험요약1', '경험요약2'],
-      capabilities: [
-        {
-          isAi: false,
-          id: 1234,
-          keyword: '문제해결력',
-        },
-        {
-          isAi: false,
-          id: 1235,
-          keyword: '협동력',
-        },
-        {
-          isAi: false,
-          id: 1236,
-          keyword: '커뮤니케이션',
-        },
-        {
-          isAi: true,
-          id: 123,
-          keyword: '창의력',
-        },
-      ],
-    },
-  ];
+  // const experiences: Experience[] = [
+  //   {
+  //     id: 1,
+  //     title: '00전자 디자인 인턴',
+  //     startDate: '2022-02-01T00:00:00.000Z',
+  //     endDate: '2022-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-01T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 124,
+  //         keyword: '데이터분석',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     title: '00물산 영업',
+  //     startDate: '2022-03-01T00:00:00.000Z',
+  //     endDate: '2022-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-02T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1237,
+  //         keyword: '도전정신',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1238,
+  //         keyword: '고객지향성',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1239,
+  //         keyword: '기획력',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 124,
+  //         keyword: '데이터분석',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '00전자 개발',
+  //     startDate: '2020-05-01T00:00:00.000Z',
+  //     endDate: '2020-08-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-03T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     title: '00기획 마케팅',
+  //     startDate: '2023-01-01T00:00:00.000Z',
+  //     endDate: '2023-05-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-04T00:00:00.000Z',
+  //     experienceStatus: 'DONE',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 5,
+  //     title: '00은행 영업',
+  //     startDate: '2022-01-01T00:00:00.000Z',
+  //     endDate: '2022-03-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-05T00:00:00.000Z',
+  //     experienceStatus: 'DONE',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 6,
+  //     title: '00디스플레이 생산관리',
+  //     startDate: '2021-02-01T00:00:00.000Z',
+  //     endDate: '2021-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-06T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 7,
+  //     title: '00바이오 연구개발',
+  //     startDate: '2020-06-01T00:00:00.000Z',
+  //     endDate: '2021-12-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-07T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 8,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 9,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 10,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 11,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 12,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 13,
+  //     title: '00방송 콘텐츠 기획',
+  //     startDate: '2020-02-01T00:00:00.000Z',
+  //     endDate: '2020-04-01T00:00:00.000Z',
+  //     updatedAt: '2023-04-08T00:00:00.000Z',
+  //     experienceStatus: 'INPROGRESS',
+  //     situation: '개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감 개발자와 협업 역량을 쌓기 위해 IT 동아리에 들어감',
+  //     summaries: ['경험요약1', '경험요약2'],
+  //     capabilities: [
+  //       {
+  //         isAi: false,
+  //         id: 1234,
+  //         keyword: '문제해결력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1235,
+  //         keyword: '협동력',
+  //       },
+  //       {
+  //         isAi: false,
+  //         id: 1236,
+  //         keyword: '커뮤니케이션',
+  //       },
+  //       {
+  //         isAi: true,
+  //         id: 123,
+  //         keyword: '창의력',
+  //       },
+  //     ],
+  //   },
+  // ];
 
   const allCapability = getAllCapability(_capabilites);
 
@@ -274,10 +435,10 @@ const Page = () => {
     setSortBy(() => (sortBy === 'EXPERIENCE_TIME' ? 'UPDATED_AT' : 'EXPERIENCE_TIME'));
   };
 
-  const _experiences =
-    selectedCapabilityId === allCapability.id ? experiences : getFilteredExperiences(experiences, selectedCapabilityId);
+  // const _experiences =
+  //   selectedCapabilityId === allCapability.id ? experiences : getFilteredExperiences(experiences, selectedCapabilityId);
 
-  const __experiences = getSortedExperiences(_experiences, sortBy);
+  // const __experiences = getSortedExperiences(_experiences, sortBy);
 
   return (
     <>
@@ -294,13 +455,13 @@ const Page = () => {
         }
       />
       <section className="mt-[24px]">
-        <ul className="grid grid-cols-3 gap-[16px]">
+        {/* <ul className="grid grid-cols-3 gap-[16px]">
           {__experiences.map((experience: Experience) => (
             <li key={experience.id}>
               <ExperienceListCard {...experience} />
             </li>
           ))}
-        </ul>
+        </ul> */}
       </section>
     </>
   );
--- src/hooks/reactQuery/experience/qeury.ts
+++ src/hooks/reactQuery/experience/qeury.ts
@@ -1,13 +1,27 @@
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
-import { EXPERIENCE_CAPABILITY_API, EXPERIENCE_COUNT_API } from '@/apis/experience/experience';
+import { EXPERIENCE_API, EXPERIENCE_CAPABILITY_API, EXPERIENCE_COUNT_API } from '@/apis/experience/experience';
 import {
   ExperienceCapabilityParams,
   ExperienceCapabilityResponse,
   ExperienceCountResponse,
+  ExperienceParams,
+  ExperienceResponse,
 } from '@/apis/experience/types/experience';
 import { EXPERIENCE_CAPABILITY_KEY, EXPERIENCE_KEY } from '@/shared/constants/querykeys';
+
+export const useGetExperiences = (
+  params?: ExperienceParams['get'],
+  options?: UseQueryOptions<ExperienceResponse, AxiosError>
+) =>
+  useQuery<ExperienceResponse, AxiosError>(
+    EXPERIENCE_KEY.list([{ ...params }]),
+    () => EXPERIENCE_API.get({ ...params }),
+    {
+      ...options,
+    }
+  );
 
 export const useGetExperienceCount = (options?: UseQueryOptions<ExperienceCountResponse['get'], AxiosError>) =>
   useQuery<ExperienceCountResponse['get'], AxiosError>(EXPERIENCE_KEY.count(), () => EXPERIENCE_COUNT_API.get(), {
