65c63ab65b639d56cee2c6a6b23b71e82002edbc
minkyung00 <mksin00@naver.com>
2023-07-03 18:06:57 +0900

    feat: 온보딩이 완료되지 않은 경우 툴팁 표시

--- src/features/resume/components/Aside/Aside.tsx
+++ src/features/resume/components/Aside/Aside.tsx
@@ -2,13 +2,17 @@
 
 import Button from '@/components/Button/Button';
 import IconPencil from '@/components/Icon/IconPencil';
+import Tooltip from '@/components/Tooltip/Tooltip';
 import { useCreateResume } from '@/hooks/reactQuery/resume/mutation';
 import { useGetResumes } from '@/hooks/reactQuery/resume/query';
+import { useUserOnboarding } from '@/shared/store/user';
 
 import Resume from './Resume/Resume';
 import ResumeListContainer from './Resume/ResumeListContainer';
 
 const Aside = () => {
+  const { resume: isResumeOnboardingComplete } = useUserOnboarding();
+
   const { mutate: createResume } = useCreateResume();
   const { data: resumeList } = useGetResumes();
 
@@ -23,9 +27,22 @@ const Aside = () => {
           <IconPencil />
           <span>내 자기소개서</span>
         </h1>
-        <Button variant="primary" size="M" onClick={handleAddFolderButtonClick}>
-          자기소개서 추가
-        </Button>
+        {isResumeOnboardingComplete ? (
+          <Button variant="primary" size="M" onClick={handleAddFolderButtonClick}>
+            자기소개서 추가
+          </Button>
+        ) : (
+          <Tooltip
+            type="strong"
+            position="left-bottom"
+            content="‘자기소개서 추가' 버튼을 눌러 작성을 시작해보세요!"
+            alwaysOpen
+            className="b1">
+            <Button variant="primary" size="M" onClick={handleAddFolderButtonClick}>
+              자기소개서 추가
+            </Button>
+          </Tooltip>
+        )}
       </header>
       <ResumeListContainer expandedResumeCount={resumeList?.length}>
         {resumeList?.map((resume) => (
