875fdd3bef312f25920ec51db50328fefc471070
minkyung00 <mksin00@naver.com>
2023-07-05 01:18:40 +0900

    docs: useResumeListWithUpdatedTitle 주석 추가

--- src/features/resume/hooks/useResumeListWithUpdatedTitle.ts
+++ src/features/resume/hooks/useResumeListWithUpdatedTitle.ts
@@ -7,13 +7,22 @@ import { useGetResumes } from '@/hooks/reactQuery/resume/query';
 import { useTitle } from '../store';
 import { ResumeData } from '../types/resume';
 
-const useResumeList = () => {
+/**
+ * Aside에서 사용하는 자기소개서 목록을 반환하는 룩입니다
+ * - 사용자가 ResumeForm에서 자기소개서 제목을 변경하면 Aside에 해당 제목으로 업데이트된 자기소개서 목록을 반환합니다.
+ * @returns resumeList Aside에서 사용하는 자기소개서 목록
+ */
+const useResumesWithUpdatedTitle = () => {
   const { questionId } = useParams();
   const currentEditingTitle = useTitle();
   const { data } = useGetResumes();
 
   const [resumeList, setResumeList] = useState<ResumeData[]>();
-
+  /**
+   * 자기소개서 목록을 업데이트합니다.
+   * - 사용자가 자기소개서 제목을 변경한 경우, 변경한 제목이 반영된 자기소개서 목록 값으로 업데이트합니다.
+   * - 사용자가 자기소개서 제목을 변경한 경우, 기존 자기소개서 목록으로 업데이트합니다.
+   */
   const updateResumeList = () => {
     const newReumeList = data?.map((resume) => {
       const currentEditingQuestion = resume.questions.find(({ id }) => id === +questionId);
@@ -39,4 +48,4 @@ const useResumeList = () => {
   };
 };
 
-export default useResumeList;
+export default useResumesWithUpdatedTitle;
