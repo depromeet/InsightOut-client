0915f199c875503e3bf8e2015aef7ffca0be6f50
minkyung00 <mksin00@naver.com>
2023-07-07 00:09:35 +0900

    refactor: getExperiencePeriod 매개변수 타입 변경

--- src/features/collection/utils/getExperiencePeriod.ts
+++ src/features/collection/utils/getExperiencePeriod.ts
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 
-const getExperiencePeriod = (startDate: string, endDate: string) => {
+import { Experience } from '../types';
+
+const getExperiencePeriod = (startDate: Experience['startDate'], endDate: Experience['endDate']) => {
   const start = dayjs(startDate).format('YYYY.MM');
   const end = dayjs(endDate).format('YYYY.MM');
   return `${start} - ${end}`;
