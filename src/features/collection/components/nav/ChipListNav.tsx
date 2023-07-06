654ed64d7329330726329c1b833b1861955840b3
minkyung00 <mksin00@naver.com>
2023-07-07 00:01:23 +0900

    refactor: ChipListNav props 추가 및 스타일 수정
    - chipSize 추가
    - changeItem 타입 변경
    - flex-shrink: 0

--- src/features/collection/components/nav/ChipListNav.tsx
+++ src/features/collection/components/nav/ChipListNav.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, ReactNode, SetStateAction } from 'react';
+import { ReactNode } from 'react';
 
 import Badge from '@/components/Badge/Badge';
 import Chip from '@/components/Chip/Chip';
@@ -7,8 +7,9 @@ import addPlusMarkOver99 from '@/shared/utils/addPlusMarkOver99';
 type Props<T> = {
   items: T[];
   selectedItem: string;
-  changeItem?: Dispatch<SetStateAction<string>>;
+  changeItem?: (keyword: string) => void;
   Right?: ReactNode;
+  chipSize?: ChipSize;
 };
 
 type Item = {
@@ -17,14 +18,14 @@ type Item = {
   keyword: string;
 };
 
-const ChipListNav = <T extends Item>({ items, selectedItem, changeItem, Right }: Props<T>) => {
+const ChipListNav = <T extends Item>({ items, selectedItem, changeItem, Right, chipSize = 'M' }: Props<T>) => {
   return (
     <section className="flex flex-row justify-between items-center my-[24px]">
-      <nav className="flex flex-row gap-[8px]">
+      <nav className="flex flex-row gap-[8px] flex-shrink-0">
         {items.map(({ id, count, keyword }: T) => (
           <li key={id} className="list-none">
             <Chip
-              size="M"
+              size={chipSize}
               variant={selectedItem === keyword ? 'secondary-pressed' : 'secondary'}
               badge={
                 count ? (
