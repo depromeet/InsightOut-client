24fb8f4a71b566a203aa97bab581c63b365173dd
minkyung00 <mksin00@naver.com>
2023-07-07 00:03:23 +0900

    feat: MyExperienceCard 키워드 api 연결

--- src/features/resume/components/MyExperienceCard/MyExperienceCard.tsx
+++ src/features/resume/components/MyExperienceCard/MyExperienceCard.tsx
@@ -1,19 +1,17 @@
-import IconCards from '@/components/Icon/IconCards';
+import PrefetchHydration from '@/components/tanstackQuery/PrefetchHydration';
+import { EXPERIENCE_CAPABILITY_KEY } from '@/shared/constants/querykeys';
 
 import ExperienceCardDetail from './ExperienceCardDetail';
 import ExperienceCardList from './ExperienceCardList';
+import MyExperienceCardHeader from './MyExperienceCardHeader';
 
 const MyExperienceCard = () => {
   return (
-    <section className="resume-section min-h-[976px] shadow-toast">
-      <header className="flex items-center gap-[6px] pt-[28px] pl-[35px] pr-[37px]">
-        <h2 className="flex gap-[6px] subhead1">
-          <IconCards />
-          <span>내 경험카드</span>
-        </h2>
-        <p className="c1 text-sub">직무역량별 경험카드로 기억을 되새긴 후 자기소개서를 작성해봐요.</p>
-        {/* <FilterChipList /> */}
-      </header>
+    <section className="resume-section h-[976px] shadow-toast">
+      {/* @ts-expect-error Server Component */}
+      <PrefetchHydration queryKey={EXPERIENCE_CAPABILITY_KEY.lists()} queryFn={() => EXPERIENCE_CAPABILITY_API.get()}>
+        <MyExperienceCardHeader />
+      </PrefetchHydration>
       <hr aria-orientation="horizontal" className="border-gray-200"></hr>
       <div className="flex">
         <ExperienceCardList />
---
+++ src/features/resume/components/MyExperienceCard/MyExperienceCardHeader.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import IconCards from '@/components/Icon/IconCards';
+import ChipListNav from '@/features/collection/components/nav/ChipListNav';
+import { Capability } from '@/features/collection/types';
+import getAllCapability from '@/features/collection/utils/getAllCapabilityBadgeItem';
+import { useGetExperienceCapabilities } from '@/hooks/reactQuery/experience/qeury';
+
+import { useCapabilityKeyword, useExperienceActions } from '../../store';
+
+const MyExperienceCardHeader = () => {
+  const selectedCapabilitykeyword = useCapabilityKeyword();
+  const { setCapabilityKeyword } = useExperienceActions();
+
+  const { data: capabilities } = useGetExperienceCapabilities();
+  const allCapability = getAllCapability(capabilities ?? []);
+  const capabilityList: Capability[] = [allCapability, ...(capabilities ?? [])];
+
+  return (
+    <header className="flex flex-col pt-[28px] pl-[35px] pr-[37px]">
+      <div className="flex items-center gap-[6px]">
+        <h2 className="flex gap-[6px] subhead1">
+          <IconCards />
+          <span>내 경험카드</span>
+        </h2>
+        <p className="c1 text-sub">직무역량별 경험카드로 기억을 되새긴 후 자기소개서를 작성해봐요.</p>
+      </div>
+      <div className="w-[100%] relative after:content-[''] after:absolute after:top-0 after:right-0 after:w-[68px] after:h-[100%] after:bg-gradient-to-l after:from-[#FFF] after:to-[rgba(255, 255, 255, 0.00))]">
+        <div className="overflow-x-scroll">
+          <ChipListNav
+            chipSize="S"
+            items={capabilityList ?? []}
+            selectedItem={selectedCapabilitykeyword}
+            changeItem={setCapabilityKeyword}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default MyExperienceCardHeader;
