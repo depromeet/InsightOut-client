d315f80eaabe40a348e55a04181598dac838702f
harseille <joonhabaak@gmail.com>
2023-07-08 17:03:21 +0900

    feat: Inprogress image 추가 반영

--- src/features/collection/components/cards/CapabilityImage.tsx
+++ src/features/collection/components/cards/CapabilityImage.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 
+import { ExperienceStatus } from '@/feature/analyze/types';
+
 import common from '../../../../../public/images/capabilites/common.png';
+import inprogress from '../../../../../public/images/capabilites/inprogress.png';
 import { CAPABILITYES_IMAGE } from '../../constants';
 
 type Props = {
   keyword: string;
   width: number;
   heigth: number;
+  experienceStatus: ExperienceStatus;
 };
 
-const ExperienceImage = ({ width, heigth, keyword }: Props) => {
-  const image = CAPABILITYES_IMAGE[keyword] || common;
+const ExperienceImage = ({ width, heigth, keyword, experienceStatus }: Props) => {
+  const image = experienceStatus === 'INPROGRESS' ? inprogress : CAPABILITYES_IMAGE[keyword] || common;
   return <Image src={image} alt="keyword" width={width} height={heigth} />;
 };
 
