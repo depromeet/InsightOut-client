2aaa77af1093782a5e269868e8b651bffd2c202c
highjoon <yundosa2@gmail.com>
2023-07-08 19:04:34 +0900

    feat: 서비스 이용 약관 및 개인정보처리방침 연결

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -6,4 +6,6 @@ export const ROUTES = {
   VERIFY: '/analyze/verify',
   COMPLETE: '/analyze/complete',
   RESUMES: '/resumes',
+  TERMS_OF_SERVICE: 'https://holly-inch-d57.notion.site/939d0171ea104886aab53840837db174',
+  PRIVACY_POLICY: 'https://holly-inch-d57.notion.site/123e4efd7e7f4de088beced76df417a7',
 } as const;
