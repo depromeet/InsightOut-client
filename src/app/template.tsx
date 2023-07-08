3202b8fb2cd36ad0ab7b6b0d1e1c0d6e9c21dcac
highjoon <yundosa2@gmail.com>
2023-07-08 18:20:54 +0900

    fix: 직무 선택 이전에 회원가입 모달 닫을 시 api 미호출

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,19 +2,16 @@
 
 import { useRouter } from 'next/navigation';
 
-import userApi from '@/apis/user/user';
 import GlobalNavigationBar from '@/components/GlobalNavigationBar/GlobalNavigationBar';
 import OnboardingProvider from '@/components/Providers/OnboardingProvider';
 import AuthProvider from '@/features/auth/components/Providers/AuthProvider';
 import { useAuthActions, useIsOpenSignUpModal, useIsRequesting, useIsSignedIn } from '@/features/auth/store';
 import { ROUTES } from '@/shared/constants/routes';
-import { useUserNickname } from '@/shared/store/user';
 
 export default function Template({ children }: StrictPropsWithChildren) {
   const isSignedIn = useIsSignedIn();
   const isRequesting = useIsRequesting();
   const isOpenSignUpModal = useIsOpenSignUpModal();
-  const nickname = useUserNickname();
   const { setIsOpenSignUpModal, setIsSignedIn } = useAuthActions();
   const router = useRouter();
 
@@ -24,7 +21,6 @@ export default function Template({ children }: StrictPropsWithChildren) {
   };
 
   const handleAbortSignUp = async () => {
-    await userApi.patch({ nickname, field: null });
     setIsSignedIn(true);
   };
 
