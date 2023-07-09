14f3f92c6fc35046ea402da1b8cdbae536e1359f
minkyung00 <mksin00@naver.com>
2023-07-09 16:51:13 +0900

    feat: 자기소개서 작성에 사용하는 NotFound 컴포넌트

---
+++ src/features/resume/components/NotFound/NotFound.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from 'react';
+
+import Image from 'next/image';
+
+import Button from '@/components/Button/Button';
+import { tw } from '@/shared/utils/tailwindMerge';
+
+import lumosNocard from '../../../../../public/images/lumos-nocard.png';
+
+type NotFoundProps = {
+  title: string;
+  description: string;
+  buttonText: string;
+  onButtonClick?: () => void;
+} & ComponentProps<'div'>;
+
+const NotFound = ({ title, description, buttonText, onButtonClick, className, ...props }: NotFoundProps) => {
+  return (
+    <div className={tw('flex flex-col items-center justify-center w-[100%] h-[100%]', className)} {...props}>
+      <Image src={lumosNocard} alt="noCard" width={225} priority />
+      <p className="subhead2 text-sub">{title}</p>
+      <p className="b1 text-light mb-[28px]">{description}</p>
+      <Button variant="secondary" size="XL" onClick={onButtonClick}>
+        {buttonText}
+      </Button>
+    </div>
+  );
+};
+
+export default NotFound;
