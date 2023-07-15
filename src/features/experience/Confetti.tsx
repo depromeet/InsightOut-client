4f08f07c2f4aa4c813bbe1890d35eeeff7964082
harseille <joonhabaak@gmail.com>
2023-07-15 15:20:14 +0900

    chore: confetti 스타일 변경

--- src/features/experience/Confetti.tsx
+++ src/features/experience/Confetti.tsx
@@ -3,6 +3,8 @@
 import { useCallback, useEffect, useRef } from 'react';
 import ReactCanvasConfetti from 'react-canvas-confetti';
 
+import { colors } from '@/styles/theme/foundations/colors';
+
 const Confetti = () => {
   const refAnimationInstance = useRef<any>(null);
 
@@ -16,50 +18,59 @@ const Confetti = () => {
         ...opts,
         zIndex: -1,
         origin: { y: 0.6 },
-        particleCount: Math.floor(500 * particleRatio),
+        particleCount: Math.floor(300 * particleRatio),
       });
   }, []);
 
   const fire = useCallback(() => {
-    makeShot(0.2, {
-      spread: 95,
-      startVelocity: 50,
-      scalar: 0.7,
-    });
+    const { primary, secondary } = colors;
+    const confettiColor = [primary[300], secondary[300], '#FF59F8'];
+    const confettiShape = ['circle', 'star', 'square'];
+    const commonConfettiOption = { confettiColor, confettiShape };
 
     makeShot(0.25, {
       spread: 100,
       startVelocity: 60,
-      scalar: 0.5,
+      scalar: 1,
+      ...commonConfettiOption,
+    });
+
+    makeShot(0.2, {
+      spread: 120,
+      startVelocity: 50,
+      scalar: 1.2,
+      ...commonConfettiOption,
     });
 
     makeShot(0.2, {
       spread: 160,
       startVelocity: 80,
-      scalar: 1.3,
+      scalar: 1.8,
       decay: 0.91,
+      ...commonConfettiOption,
     });
 
     makeShot(0.25, {
       spread: 260,
       decay: 0.94,
-      scalar: 1,
+      scalar: 1.5,
+      ...commonConfettiOption,
     });
 
     makeShot(0.2, {
       spread: 280,
       startVelocity: 25,
       decay: 0.94,
-      scalar: 0.8,
-      shapes: ['circle', 'star', 'square'],
+      scalar: 1.3,
+      ...commonConfettiOption,
     });
 
     makeShot(0.1, {
       spread: 360,
       startVelocity: 45,
       decay: 0.93,
-      scalar: 0.9,
-      shapes: ['circle', 'star', 'square'],
+      scalar: 1.4,
+      ...commonConfettiOption,
     });
   }, [makeShot]);
 
