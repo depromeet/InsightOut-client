25c1c4a553d8dbcf48157dfa59c59f65c281f72d
harseille <joonhabaak@gmail.com>
2023-07-16 18:52:32 +0900

    feat: layout에 불필요한 코드 정리

--- src/app/completed-experience-card/layout.tsx
+++ src/app/completed-experience-card/layout.tsx
@@ -1,24 +1,9 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-
-import Confetti from '@/features/experience/Confetti';
 import Wrapper from '@/features/experience/Wrapper';
 
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
-const layout = ({ children }: LayoutProps) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const experienceId = useSearchParams().get('experienceId');
-
-  return (
-    <Wrapper>
-      {children}
-      <Confetti />
-    </Wrapper>
-  );
-};
+const layout = ({ children }: LayoutProps) => <Wrapper>{children}</Wrapper>;
 
 export default layout;
