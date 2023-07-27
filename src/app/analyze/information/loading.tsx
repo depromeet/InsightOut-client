afd3803943f26edeb8de2651ebf7ac621808f65c
sangbooom <tkdqja4164@naver.com>
2023-07-28 00:12:34 +0900

    refactor: 경험분해 작성 페이지에 로딩 컴포넌트 추가

---
+++ src/app/analyze/information/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+import PageLoading from '@/features/analyze/PageLoading/PageLoading';
+
+const loading = () => {
+  return <PageLoading />;
+};
+
+export default loading;
