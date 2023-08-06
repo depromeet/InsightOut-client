40a9cc33477a561172c2fb948c834366cd270623
sangbooom <tkdqja4164@naver.com>
2023-08-07 00:00:30 +0900

    feat: yup schema 작성

--- src/features/ExperienceFormProvider.tsx
+++ src/features/ExperienceFormProvider.tsx
@@ -4,13 +4,15 @@ import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { DevTool } from '@hookform/devtools';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter } from 'next/navigation';
+import * as yup from 'yup';
 
 import { useIsMounted } from '@/hooks/useIsMounted';
 import { useExperienceId } from '@/shared/store/experienceId';
 
 import { initialValue } from './analyze/constants';
-import { ExperienceFormValues } from './analyze/types';
+import { CapabilitiesType, ExperienceFormValues, KeywordEntriesType, WriteStatusType } from './analyze/types';
 
 const ExperienceFormProvider = ({ children }: StrictPropsWithChildren) => {
   const { push } = useRouter();
@@ -19,6 +21,7 @@ const ExperienceFormProvider = ({ children }: StrictPropsWithChildren) => {
   const methods = useForm<ExperienceFormValues>({
     mode: 'all',
     defaultValues: initialValue,
+    resolver: yupResolver(schema),
   });
   const { experienceId } = useExperienceId();
 
@@ -41,3 +44,43 @@ const ExperienceFormProvider = ({ children }: StrictPropsWithChildren) => {
 };
 
 export default ExperienceFormProvider;
+
+const yearSchema = yup.string().optional().min(4, '연도를 입력해주세요 (ex. 2023)');
+const monthSchema = yup
+  .string()
+  .optional()
+  .min(2, '월을 입력해주세요 (ex. 09)')
+  .matches(/(0[1-9]|1[0-2])/, '정확한 날짜를 입력해주세요');
+
+const schema = yup.object().shape({
+  experienceId: yup.number().required(),
+  title: yup.string().required(),
+  experienceRole: yup.string().required(),
+  motivation: yup.string().required(),
+
+  startYYYY: yearSchema,
+  startMM: monthSchema,
+  startDate: yup.string().required(),
+
+  endYYYY: yearSchema.test('dateRules', '종료 기간은 시작 기간과 같거나 이후여야 합니다', (value, context) => {
+    const startYYYY = context.resolve(yup.ref<string>('startYYYY'));
+    if (!value?.trim() || !startYYYY) return;
+    return value.length === 4 && startYYYY.length === 4 && value >= startYYYY;
+  }),
+  endMM: monthSchema.test('dateRules', '종료 기간은 시작 기간과 같거나 이후여야 합니다', (value, context) => {
+    const startMM = context.resolve(yup.ref<string>('startMM'));
+    if (!value?.trim() || !startMM) return;
+    return value.length === 2 && startMM.length === 2 && value >= startMM;
+  }),
+  endDate: yup.string().required(),
+
+  keywords: yup.array<KeywordEntriesType>().required(),
+  situation: yup.string().required(),
+  task: yup.string().required(),
+  action: yup.string().required(),
+  result: yup.string().required(),
+  experienceStatus: yup.string().required(),
+  writeStatus: yup.array<WriteStatusType[]>().optional(),
+  capabilities: yup.array<CapabilitiesType[]>().required(),
+  resume: yup.string().required(),
+});
