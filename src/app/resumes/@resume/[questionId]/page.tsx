fb388a8646089d15c003bb2282fcb60fed542e6b
minkyung00 <mksin00@naver.com>
2023-08-10 20:24:50 +0900

    refactor: Resume에 parallel routing 적용

---
+++ src/app/resumes/@resume/[questionId]/page.tsx
@@ -0,0 +1,19 @@
+import questionApi from '@/apis/resume/question';
+import PrefetchHydration from '@/components/tanstackQuery/PrefetchHydration';
+import ResumeForm from '@/features/resume/components/ResumeForm/ResumeForm';
+import { QUESTION_KEY } from '@/shared/constants/querykeys';
+
+const Page = ({ params: { questionId } }: { params: { questionId: string } }) => {
+  return (
+    <div className="w-[700px] h-max min-h-[976px] border-[1px] border-purple-100 shadow-toast rounded-3xl bg-white py-[30px] px-11">
+      {/* @ts-expect-error Server Component */}
+      <PrefetchHydration
+        queryKey={QUESTION_KEY.detail([{ questionId }])}
+        queryFn={() => questionApi.get({ questionId })}>
+        <ResumeForm questionId={+questionId} />
+      </PrefetchHydration>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/resumes/layout.tsx
+++ src/app/resumes/layout.tsx
@@ -2,15 +2,16 @@ import { PropsWithChildren, ReactNode } from 'react';
 
 type LayoutProps = PropsWithChildren & {
   aside: ReactNode;
+  resume: ReactNode;
   myExperienceCard: ReactNode;
 };
 
-const Layout = ({ children, aside, myExperienceCard }: LayoutProps) => {
+const Layout = ({ aside, resume, myExperienceCard }: LayoutProps) => {
   return (
     <div className="flex h-full max-[1536px]:h-max bg-gray-50 pt-[56px]">
       {aside}
       <div className="flex mt-[19px] gap-[20px] px-[10px] max-[1536px]:flex-col max-[1536px]:pl-[101px] ">
-        {children}
+        {resume}
         {myExperienceCard}
       </div>
     </div>
