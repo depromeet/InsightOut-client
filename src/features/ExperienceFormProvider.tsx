edab7e5d6d343becd6c6778bdf55c7705dd7c5b1
sangbooom <tkdqja4164@naver.com>
2023-08-20 15:31:31 +0900

    refactor: schema validation 수정

--- src/features/ExperienceFormProvider.tsx
+++ src/features/ExperienceFormProvider.tsx
@@ -60,7 +60,6 @@ const schema = yup.object().shape({
 
   startYYYY: yearSchema,
   startMM: monthSchema,
-  startDate: yup.string().required(),
 
   endYYYY: yearSchema.test('dateRules', '종료 기간은 시작 기간과 같거나 이후여야 합니다', (value, { resolve }) => {
     const startYYYY = resolve(yup.ref<string>('startYYYY'));
@@ -72,7 +71,6 @@ const schema = yup.object().shape({
     if (!value?.trim() || !startMM) return;
     return value.length === 2 && startMM.length === 2 && value >= startMM;
   }),
-  endDate: yup.string().required(),
 
   keywords: yup.mixed<KeywordEntriesType>().required(),
   situation: yup.string().required(),
@@ -82,5 +80,5 @@ const schema = yup.object().shape({
   experienceStatus: yup.string().required(),
   writeStatus: yup.mixed<WriteStatusType[]>().optional(),
   capabilities: yup.mixed<CapabilitiesType[]>().required(),
-  resume: yup.string().required(),
+  aiResume: yup.string().required(),
 });
