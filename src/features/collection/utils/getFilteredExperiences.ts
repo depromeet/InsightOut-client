3753849de43154b210cb116c0072eb2dcafdc0f8
minkyung00 <mksin00@naver.com>
2023-10-01 16:05:56 +0900

    fix: 경험카드 필터링 로직 수정

--- src/features/collection/utils/getFilteredExperiences.ts
+++ src/features/collection/utils/getFilteredExperiences.ts
@@ -1,7 +1,10 @@
 import { Experience } from '../types';
 
-const getFilteredExperiences = (experiences: Experience[], selectedCapability: string) =>
-  experiences.filter((experience) =>
+const getFilteredExperiences = (experiences: Experience[], selectedCapability: string) => {
+  if (selectedCapability === '전체') return experiences;
+
+  return experiences.filter((experience) =>
     experience.experienceCapabilityKeywords?.find((capability) => capability === selectedCapability)
   );
+};
 export default getFilteredExperiences;
